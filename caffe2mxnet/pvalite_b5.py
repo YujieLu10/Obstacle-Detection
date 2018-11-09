9be5221986cd28b255c413399358b0242cb9adfd
YUJIE LU <18867111045@163.com>
2018-11-09 10:57:18 +0800

    Update pvalite_b5.py
    fix bias concat layer and hidden filter

--- caffe2mxnet/pvalite_b5.py
+++ caffe2mxnet/pvalite_b5.py
@@ -26,22 +26,22 @@ class pvalite_b5(Symbol):
         return ['bbox_pred_weight', 'bbox_pred_bias']
 
     def inc3_unit_left(self, data, name, workspace=512):
-        conv5_1 = mx.sym.Convolution(data=data,bias=0.1,num_filter=16,kernel=(1,1),stride=(1,1),pad=0,no_bias=False,workspace=workspace,name=name+'/conv5_1')
+        conv5_1 = mx.sym.Convolution(data=data,num_filter=16,kernel=(1,1),stride=(1,1),pad=0,no_bias=False,workspace=workspace,name=name+'/conv5_1')
         relu5_1 = mx.sym.Activation(data=conv5_1, act_type='relu', name=name+'/relu5_1')
         if 'inc4' in name or 'inc5' in name:
-            conv5_2 = mx.sym.Convolution(data=relu5_1,bias=0.1, num_filter=32,kernel=(3,3),stride=(1,1),dilate=(2,2),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_2')
+            conv5_2 = mx.sym.Convolution(data=relu5_1, num_filter=32,kernel=(3,3),stride=(1,1),dilate=(2,2),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_2')
             relu5_2 = mx.sym.Activation(data=conv5_2, act_type='relu', name=name+'/relu5_2')
-            conv5_3 = mx.sym.Convolution(data=relu5_2,bias=0.1, num_filter=32,kernel=(3,3),stride=(1,1),dilate=(2,2),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_3')
+            conv5_3 = mx.sym.Convolution(data=relu5_2, num_filter=32,kernel=(3,3),stride=(1,1),dilate=(2,2),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_3')
             relu5_3 = mx.sym.Activation(data=conv5_3, act_type='relu', name=name+'/relu5_3')
         else:
-            conv5_2 = mx.sym.Convolution(data=relu5_1,bias=0.1, num_filter=32,kernel=(3,3),stride=(1,1),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_2')
+            conv5_2 = mx.sym.Convolution(data=relu5_1, num_filter=32,kernel=(3,3),stride=(1,1),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_2')
             relu5_2 = mx.sym.Activation(data=conv5_2, act_type='relu', name=name+'/relu5_2')
-            conv5_3 = mx.sym.Convolution(data=relu5_2,bias=0.1, num_filter=32,kernel=(3,3),stride=(1,1),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_3')
+            conv5_3 = mx.sym.Convolution(data=relu5_2, num_filter=32,kernel=(3,3),stride=(1,1),pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv5_3')
             relu5_3 = mx.sym.Activation(data=conv5_3, act_type='relu', name=name+'/relu5_3')
         return relu5_3
 
     def inc3_unit_middle(self, data, name, workspace=512):
-        conv3_1 = mx.sym.Convolution(data=data, bias=, num_filter=16,kernel=(1,1),stride=(1,1),pad=0,no_bias=False,workspace=workspace,name=name+'/conv3_1')
+        conv3_1 = mx.sym.Convolution(data=data, num_filter=16,kernel=(1,1),stride=(1,1),pad=0,no_bias=False,workspace=workspace,name=name+'/conv3_1')
         relu3_1 = mx.sym.Activation(data=conv3_1,act_type='relu', name=name+'/relu3_1')
 
         if name == 'inc3a':
@@ -54,9 +54,10 @@ class pvalite_b5(Symbol):
         else:        
             conv3_2 = mx.sym.Convolution(data=relu3_1, num_filter=64,kernel=(3,3),stride=incstride,pad=(1,1),no_bias=False,workspace=workspace,name=name+'/conv3_2')
             relu3_2 = mx.sym.Activation(data=conv3_2,act_type='relu', name=name+'/relu3_2')
+        return relu3_2
 
     def inc3_unit_right(self, data, num_output, name, workspace=512):
-        conv1 = mx.sym.Convolution(data=data, num_filter=num_output,kernel=,stride=,pad=,no_bias=False,workspace=workspace,name=name+'/conv1')
+        conv1 = mx.sym.Convolution(data=data, num_filter=num_output,kernel=(1,1),stride=(1,1),pad=(0,0),no_bias=False,workspace=workspace,name=name+'/conv1')
         relu1 = mx.sym.Activation(data=conv1, act_type='relu', name=name+'/relu1')
         return relu1
 
@@ -94,18 +95,20 @@ class pvalite_b5(Symbol):
         inc3_right = conv3
         conv3_down2 = mx.symbol.Pooling(data=conv3, kernel=(3, 3), stride=(2, 2), pad=(0, 0), pool_type='max')
         #incleft
-        for i in range(num_stage - 1):
+        for i in range (num_stage):
+            print(stage_num[i])
             #inc3/pool
             if i == 0:
                 inc3_right = mx.symbol.Pooling(data=inc3_right, kernel=(2 if i == 0 else 3, 2 if i == 0 else 3), stride=(2 if i == 0 else 1, 2 if i == 0 else 1), pad=(0 if i == 0 else 1, 0 if i == 0 else 1), pool_type='max')
             else:
                 inc3_right = mx.symbol.Pooling(data=inc_concat, kernel=(2 if i == 0 else 3, 2 if i == 0 else 3), stride=(2 if i == 0 else 1, 2 if i == 0 else 1), pad=(0 if i == 0 else 1, 0 if i == 0 else 1), pool_type='max')
-            for j in range(char_stage - 1):
+            for j in range (char_stage):
+                print(stage_char[j])
                 inc3_left = self.inc3_unit_left(inc3_left if i == 0 else inc_concat, 'inc' + stage_num[i] + stage_char[j], workspace)
                 inc3_middle = self.inc3_unit_middle(inc3_middle if i == 0 else inc_concat, 'inc' + stage_num[i] + stage_char[j], workspace)
                 inc3_right = self.inc3_unit_right(inc3_right if i == 0 else inc_concat, filter_list[i], 'inc' + stage_num[i] + stage_char[j], workspace)
-                inc_concat = mx.sym.concat(inc3_left, inc3_middle, inc3_right)
-
+                inc_concat = mx.sym.concat(inc3_left, inc3_middle, inc3_right, dim=3)
+                #inc_concat = mx.sym.concat()
                 if stage_char == 'e' and stage_num == '3':
                     inc3e = inc_concat
                 
@@ -139,7 +142,7 @@ class pvalite_b5(Symbol):
         
         #batchsize?? self.test_nbatch
         proposal, rpn_scores = mx.sym.MultiProposal(cls_prob=rpn_cls_prob_reshape,bbox_pred=rpn_bbox_pred_fabu, im_info=im_info,name='proposal', batch_size=16,
-        rpn_pre_nms_top_n=10000,rpn_post_nms_top_n=2000, rpn_min_size=8, threshold=,feature_stride=16,ratios=(0.333, 0.5, 0.667, 1, 1.5, 2, 3),scales=(1.5, 3, 6, 9, 16, 32, 48))
+        rpn_pre_nms_top_n=10000,rpn_post_nms_top_n=2000, rpn_min_size=8, threshold=0.7,feature_stride=16,ratios=(0.333, 0.5, 0.667, 1, 1.5, 2, 3),scales=(1.5, 3, 6, 9, 16, 32, 48))
 
         #rpn_loss_cls =
         #mute_rpn_scores = ?
